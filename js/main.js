ec4603825cba6a61145f609625d246ab19582cbd
edwinpuco <edwin22@live.com>
2018-12-12 16:31:41 -0500

    inicio de JQuery en el resumen de helado1, helado2, helado2

--- js/main.js
+++ js/main.js
@@ -13,3 +13,20 @@
         //.openTooltip()
   });//DOM CONTENT LOADED
 })();
+
+$(function(){
+  //alert("funciona");
+  //programa de helados
+  //$('div.ocultar').hide();
+  $('.programa-evento .info-curso:first').show();
+  $('.menu-programa a:first').addClass('activo');
+  $('.menu-programa a').on('click',function(){
+    $('.menu-programa a').removeClass('activo');
+    $(this).addClass('activo');
+    $('.ocultar').hide();
+    var enlace=$(this).attr('href');
+    $(enlace).fadeIn(1000);
+    //console.log(enlace);
+    return false;
+  });
+});
