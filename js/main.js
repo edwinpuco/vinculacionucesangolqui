04ede1585cf8c4fd1cb82f78a56ade40adc17f72
edwinpuco <edwin22@live.com>
2018-12-12 22:51:13 -0500

    correción de la ubicación en el mapa

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
   "use strict";
   document.addEventListener('DOMContentLoaded', function(){
     //console.log("listo");
-    var map = L.map('mapa').setView([-0.303356, -78.454543], 16);
+    var map = L.map('mapa').setView([-0.316435, -78.458774], 17);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-    L.marker([-0.303356, -78.454543]).addTo(map)
+    L.marker([-0.316435, -78.458774]).addTo(map)
         .bindPopup('Life Artesanal.<br> Tu heladería.')
         .openPopup();
         //.bindTooltip('un tooltip')
